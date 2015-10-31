c08a069adf7ab952e07282c89df93a755d2864c4
yhf <xdhfying@gmail.com>
2015-10-31 17:30:45 -0400

    complete Mapper and Reducer

--- ClusterToPointReducer.java
+++ ClusterToPointReducer.java
@@ -9,6 +9,30 @@ import java.io.IOException;
  * You can modify this class as you see fit, as long as you correctly update the
  * global centroids.
  */
-public class ClusterToPointReducer extends Reducer<Text, Text, Text, Text>
-{
+public class ClusterToPointReducer extends Reducer<IntWritable, Point, IntWritable, Point> {
+
+    protected void reduce(IntWritable key, Iterable<Point> values, Context context) throws IOException, InterruptedException {
+        List<Float> sumCoordinates = new ArrayList<>();
+        List<Point> points = new ArrayList<>();
+        for (Point point : values) {
+            points.add(new Point(point));
+            for (int i = 0; i < point.getDimension(); i++) {
+                if (i >= sumCoordinates.size()) {
+                    sumCoordinates.add(point.getCoordinates().get(i));
+                } else {
+                    sumCoordinates.set(i, sumCoordinates.get(i) + point.getCoordinates().get(i));
+                }
+            }
+        }
+        for (int i = 0; i < sumCoordinates.size(); i++) {
+            sumCoordinates.set(i, sumCoordinates.get(i) / (float) points.size());
+        }
+        Point newCentroid = new Point(sumCoordinates);
+
+        KMeans.centroids.set(key.get(), newCentroid);
+        for (Point point : points) {
+            context.write(key, point);
+        }
+
+    }
 }
--- PointToClusterMapper.java
+++ PointToClusterMapper.java
@@ -9,6 +9,26 @@ import java.io.IOException;
  * You can modify this class as you see fit.  You may assume that the global
  * centroids have been correctly initialized.
  */
-public class PointToClusterMapper extends Mapper<Text, Text, Text, Text>
-{
+public class PointToClusterMapper extends Mapper<Text, Text, IntWritable, Point> {
+
+    @Override
+    protected void map(Text key, Text value, Context context) throws IOException, InterruptedException {
+        Point nearest = null;
+        float nearestDistance = Float.MAX_VALUE;
+
+        Point point = new Point(key.toString());
+        int centroidIndex = 0;
+
+        for (int i = 0; i < KMeans.centroids.size(); i++) {
+            Point centroid = KMeans.centroids.get(i);
+            float distance = Point.distance(centroid, point);
+            if (nearest == null || distance < nearestDistance) {
+                nearest = centroid;
+                centroidIndex = i;
+                nearestDistance = distance;
+            }
+        }
+
+        context.write(new IntWritable(centroidIndex), point);
+    }
 }
